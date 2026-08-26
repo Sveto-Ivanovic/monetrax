f819e119089bb9062963f366db2a4c0a4f926dd9
Sveto-Ivanovic <svetoivanovic788@gmail.com>
2026-08-26 22:13:34 +0200

    inital commit

---
+++ be/src/test/java/com/monetrax/monetrax/MonetraxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monetrax.monetrax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonetraxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
